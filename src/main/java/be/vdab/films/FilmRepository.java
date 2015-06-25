456522e7295cdd9b0a987fabb118077680e87155
Noor Anthonis <noor.anthonis@gmail.com>
2015-06-25 13:42:08 +0200

    some configuration changes

---
+++ src/main/java/be/vdab/films/FilmRepository.java
@@ -0,0 +1,10 @@
+package be.vdab.films;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FilmRepository extends JpaRepository<Film, Integer> {
+
+
+}
